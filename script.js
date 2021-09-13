029568d119d1753c7456a726d895cc683b7d0d97
Syb Scholing <74767501+sybroo@users.noreply.github.com>
2021-09-13 10:45:10 +0000

    alles gedaan mar trillende bal

--- script.js
+++ script.js
@@ -1,63 +1,38 @@
-var x, y, v
-
  class Ball{
    constructor(x,y,w,h,vx,vy){
-   this.x = x;
-   this.y = y;
-   this.width = w;
-   this.height = h;
-   this.vx = vw;
-   this.vy = vy;
+    this.x = x;
+    this.y = y;
+    this.width = w;
+    this.height = h;
+    this.vx = vx;
+    this.vy = vy;
+   }
 
+  drawBall(){
+    fill(255)
+    ellipse(this.x,this.y,this.width,this.height)
+    this.x = this.x + this.vx;
+    this.y = this.x + this.vy;
+
+  if(this.x <=0 || this.x >=500){
+      this.vx = -this.vx
+      }
+
+  if(this.y <=0 || this.y >=400){
+      this.vy = -this.vy
+      }
+
+  }
+ }
+
+
+function setup(){
+  createCanvas (500,400);
+ ball1 = new Ball(10,10,50,50,5,5)
 }
-ball(this.x,this.y,this.width,this.height,this.vx,this.vy)
 
-
-drawball()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function setup() {
-	createCanvas(500, 400);
-function draw() {
-	background(0);
-  
-  ellipse(x,y,70,70);
-  x = x + vx;
-  y = y + vy;
-  
-  if(x <= 0 || x >=500){
-    vx = vx * -1
-  }
-
-  if(y <= 0 || y >=400){
-    vy = vy * -1
-  }
-  
-ellipse(s,p,70,70);
-  s = s + vs;
-  p = p + vp;
-  
-  if(s <= 0 || s >=500){
-    vs = vs * -1
-  }
-
-  if(p <= 0 || p >=400){
-    vp = vp * -1
-  }
+function draw(){
+ background(100);
+ ball1.drawBall();
 
 }
